9a704b94db36676df280727ca0fa6e35407dbc1e
slepo <slepo@DESKTOP-PRFHRHE>
2024-11-02 21:50:49 +0100

    D

--- Projekt_1/p3_odpowiedz_skokowa.m
+++ Projekt_1/p3_odpowiedz_skokowa.m
@@ -2,10 +2,10 @@ function [y,u] = p3_odpowiedz_skokowa(kk)
 %P3_ODPOWIEDZ_SKOKOW Summary of this function goes here
 %   Detailed explanation goes here
 kk = kk +1;
-Ypp=2; Upp=1.2; dU=0.3;
+Ypp=2; Upp=1.2; dU=0.05;
 
 % kk=150; % koniec symulacji
-u(1:kk)=Upp+dU; y(1:11)=2;
+u(1:kk)=Upp+dU; y(1:11)=2; y(12:kk)=0;
 
 % główna pętla symulacyjna
 for k=12:kk
