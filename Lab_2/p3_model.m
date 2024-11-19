56899b2c660bb039f77bded8ecba99bb401c45a5
rslepowr <01178255@pw.edu.pl>
2024-11-19 17:50:34 +0100

    poprawienie indeksów w generacji odp skok

--- Lab_2/p3_model.m
+++ Lab_2/p3_model.m
@@ -8,9 +8,13 @@ a2 = alfa1*alfa2;
 b1 = (K/(T1-T2))*(T1*(1-alfa1)-T2*(1-alfa2));
 b2 = (K/(T1-T2))*(alfa1*T2*(1-alfa2)-alfa2*T1*(1-alfa1));
 
-y = zeros(kk+1,1);
-for k=Td+3:kk+1
+kk = kk+Td;
+
+u(1:Td+2)=0; u(Td+3:kk)=1;
+
+y = zeros(kk,1);
+for k=3+Td:kk
     y(k) = b1*u(k-Td-1) + b2*u(k-Td-2) - a1*y(k-1) - a2*y(k-2);
 end
-y(1)=[];
+y(1:Td+3)=[];
 end
